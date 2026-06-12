9f7686fbc4c1f2ba35c5188d36d0a263edc97f78
Vivien Garcia <viviengarcia@users.noreply.github.com>
2026-06-12 13:50:49 +0200

    fix: remove useless definitions

--- config/defaults.h
+++ config/defaults.h
@@ -1,29 +1,29 @@
-// Timing is Key!
+// // Timing is Key!
 
-#ifndef TAPPING_TERM
-#define TAPPING_TERM 300
-#endif
+// #ifndef TAPPING_TERM
+// #define TAPPING_TERM 300
+// #endif
 
-#ifndef SHORT_TAPPING_TERM
-#define SHORT_TAPPING_TERM 150
-#endif
+// #ifndef SHORT_TAPPING_TERM
+// #define SHORT_TAPPING_TERM 150
+// #endif
 
-#ifndef QUICK_TAP
-#define QUICK_TAP 200
-#endif
+// #ifndef QUICK_TAP
+// #define QUICK_TAP 200
+// #endif
 
-// Hold-Tap Flavor
+// // Hold-Tap Flavor
 
-#ifdef FOUR_THUMB_KEYS
-  #undef HT_NONE
-  #undef HT_THUMB_TAPS
-  #undef HT_HOME_ROW_MODS
-  #define HT_TWO_THUMB_KEYS
-#elif !defined HT_NONE && !defined HT_THUMB_TAPS && !defined HT_HOME_ROW_MODS && !defined HT_TWO_THUMB_KEYS
-  #define HT_HOME_ROW_MODS
-#elif defined HT_NONE + defined HT_THUMB_TAPS + defined HT_HOME_ROW_MODS + defined HT_TWO_THUMB_KEYS > 1
-  #error "Please select only up to one hold-tap configuration at a time"
-#endif
+// #ifdef FOUR_THUMB_KEYS
+//   #undef HT_NONE
+//   #undef HT_THUMB_TAPS
+//   #undef HT_HOME_ROW_MODS
+//   #define HT_TWO_THUMB_KEYS
+// #elif !defined HT_NONE && !defined HT_THUMB_TAPS && !defined HT_HOME_ROW_MODS && !defined HT_TWO_THUMB_KEYS
+//   #define HT_HOME_ROW_MODS
+// #elif defined HT_NONE + defined HT_THUMB_TAPS + defined HT_HOME_ROW_MODS + defined HT_TWO_THUMB_KEYS > 1
+//   #error "Please select only up to one hold-tap configuration at a time"
+// #endif
 
 // Memory
 
